afb472ebc14d0fec6386e5bf7b88ea4af08eaeb6
Seun Fadeyi <seunfadeyi@gmail.com>
2019-10-30 02:59:28 +0100

    Add files via upload
    initial commit

---
+++ dist/js/about.f789fb9a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("Header")],1)},o=[],s=t("0418"),u={name:"home",components:{Header:s["a"]}},c=u,l=t("2877"),r=Object(l["a"])(c,a,o,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=about.f789fb9a.js.map
